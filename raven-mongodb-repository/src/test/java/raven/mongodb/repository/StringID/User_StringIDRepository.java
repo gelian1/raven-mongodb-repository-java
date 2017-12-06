0ea2b873e3a6873b7bd01308910013ef894682f0
gelian <gelian@mallcoo.cn>
2017-12-06 22:31:16 +0800

    test调整

--- raven-mongodb-repository/src/test/java/raven/mongodb/repository/StringID/User_StringIDRepository.java
+++ raven-mongodb-repository/src/test/java/raven/mongodb/repository/StringID/User_StringIDRepository.java
@@ -5,7 +5,7 @@ import raven.mongodb.repository.MongoRepositoryImpl;
 
 public class User_StringIDRepository extends MongoRepositoryImpl<User_StringID,String> {
     public User_StringIDRepository(){
-        super("mongodb://218.244.136.30:27018/"
+        super("mongodb://127.0.0.1:27017/"
                 , "RepositoryTest2", null, WriteConcern.ACKNOWLEDGED, null, null);
     }
 }
